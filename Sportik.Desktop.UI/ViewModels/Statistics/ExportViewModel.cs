125b30a0d94a3fccba7ba30916b83c5ebbf2832d
Yaroslav Yuriichuk <yuriichukyaroslav@gmail.com>
2026-07-13 20:46:37 +0200

    Implement basic UI

---
+++ Sportik.Desktop.UI/ViewModels/Statistics/ExportViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sportik.Desktop.UI.ViewModels.Statistics
+{
+    internal sealed class ExportViewModel : ViewModel
+    {
+        private bool _isOpen;
+
+        public bool IsOpen
+        {
+            get => _isOpen;
+            private set => SetField(ref _isOpen, value);
+        }
+
+        private string _googleSheetId;
+
+        public string GoogleSheetId
+        {
+            get => _googleSheetId;
+            set => SetField(ref _googleSheetId, value);
+        }
+
+        public ReactiveRelayCommand ExportCommand { get; }
+        public ReactiveRelayCommand CloseCommand { get; }
+
+        private readonly CancellationTokenSource _exportCts = new CancellationTokenSource();
+
+        public ExportViewModel()
+        {
+            ExportCommand = new ReactiveRelayCommand(Export);
+            CloseCommand = new ReactiveRelayCommand(Close);
+        }
+
+        public void Dispose()
+        {
+            _exportCts.Cancel();
+            _exportCts.Dispose();
+        }
+
+        public void Open()
+        {
+            IsOpen = true;
+        }
+
+        private void Export()
+        {
+            _ = ExportAsync(_exportCts.Token);
+        }
+
+        private void Close()
+        {
+            IsOpen = false;
+        }
+
+        private async Task ExportAsync(CancellationToken cancellationToken)
+        {
+            ExportCommand.IsExecutable = false;
+            CloseCommand.IsExecutable = false;
+
+            ExportCommand.IsExecutable = true;
+            CloseCommand.IsExecutable = true;
+        }
+    }
+}
--- Sportik.Desktop.UI/ViewModels/Statistics/StatisticsViewModel.cs
+++ Sportik.Desktop.UI/ViewModels/Statistics/StatisticsViewModel.cs
@@ -26,7 +26,11 @@ namespace Sportik.Desktop.UI.ViewModels.Statistics
 
         public ImportViewModel Import { get; } = new ImportViewModel();
 
+        public ExportViewModel Export { get; } = new ExportViewModel();
+
         public ICommand ShowImportCommand { get; }
+
+        public ICommand ShowExportCommand { get; }
 
         private IExerciseStatisticsService ExerciseStatisticsService => App.ServiceProvider.GetRequiredService<IExerciseStatisticsService>();
         private IEventsService EventsService => App.ServiceProvider.GetRequiredService<IEventsService>();
@@ -36,6 +40,7 @@ namespace Sportik.Desktop.UI.ViewModels.Statistics
         public StatisticsViewModel()
         {
             ShowImportCommand = new ReactiveRelayCommand(OpenImport);
+            ShowExportCommand = new ReactiveRelayCommand(OpenExport);
 
             _ = LoadDayStatisticsAsync(_loadCts.Token);
 
@@ -54,6 +59,7 @@ namespace Sportik.Desktop.UI.ViewModels.Statistics
             }
 
             Import.Dispose();
+            Export.Dispose();
         }
 
         private async Task LoadDayStatisticsAsync(CancellationToken cancellationToken)
@@ -75,6 +81,11 @@ namespace Sportik.Desktop.UI.ViewModels.Statistics
         private void OpenImport()
         {
             Import.Open();
+        }
+
+        private void OpenExport()
+        {
+            Export.Open();
         }
 
         private void EventsService_Event(ExerciseSetAddedEventArgs args)
